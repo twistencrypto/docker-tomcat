e18839168c66dfb3892e0462a0086b7879a94476
Andrés Cuadros Suárez <andres.cuadros@energyavm.es>
2016-10-02 15:33:39 +0200

    Stable tomcat 8.5 version

---
+++ 8.5/centos7-jre8/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# remove previous images and containers
+docker ps -a | grep acs/tomcat:8.5-centos7-jre8 | awk '{print $1}' | xargs docker rm
+docker rmi -f acs/tomcat:8.5-centos7-jre8
+
+# build the image
+docker build --tag="acs/tomcat:8.5-centos7-jre8" .
